2ffaea2938abff4fca16df6b62aebc15c8b9e19a
Sweeney <ryan@squarecrop.com>
2017-02-05 17:20:04 -0500

    tutorial step 7 - http

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,20 +1,16 @@
-// Keep the Input import for now, we'll remove it later:
-import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute, Params }   from '@angular/router';
-import { Location }                 from '@angular/common';
-
+import 'rxjs/add/operator/switchMap';
+import { Component, OnInit }      from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location }               from '@angular/common';
 import { Hero }        from './hero';
 import { HeroService } from './hero.service';
-
-import 'rxjs/add/operator/switchMap';
-
 @Component({
     moduleId: module.id,
     selector: 'my-hero-detail',
     templateUrl: './hero-detail.component.html',
 })
 export class HeroDetailComponent implements OnInit {
-    @Input() hero: Hero;
+    hero: Hero;
     constructor(
         private heroService: HeroService,
         private route: ActivatedRoute,
@@ -25,8 +21,11 @@ export class HeroDetailComponent implements OnInit {
             .switchMap((params: Params) => this.heroService.getHero(+params['id']))
             .subscribe(hero => this.hero = hero);
     }
+    save(): void {
+        this.heroService.update(this.hero)
+            .then(() => this.goBack());
+    }
     goBack(): void {
         this.location.back();
     }
-
 }
